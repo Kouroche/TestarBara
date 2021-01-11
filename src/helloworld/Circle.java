4e872939fc1337aa2ada0c73606a733199d60162
Kouroche <73692398+Kouroche@users.noreply.github.com>
2021-01-11 19:48:30 +0100

    Testade med att lägga en kommetar på rad1

---
+++ src/helloworld/Circle.java
@@ -0,0 +1,23 @@
+// test
+package helloworld;
+
+/**
+ *
+ * @author crush
+ */
+public class Circle {
+
+    double radius;
+    String color;
+    
+    void display(){
+        
+        String msg="I am Circle";
+    
+        System.out.println(msg);
+        System.out.println(msg.substring(3));
+        System.out.println(msg.substring(3).toUpperCase());
+        
+    
+    }
+}
